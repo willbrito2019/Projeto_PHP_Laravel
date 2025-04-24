9a4a106afa762f9222c8543f18b05454866e7e1c
willbrito2019 <46767111+willbrito2019@users.noreply.github.com>
2025-04-23 21:01:44 -0300

    Delete orders-service directory
    Subiu por engano de outro projeto

--- orders-service/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/app');
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Order Service running on port ${PORT}`);
-});
--- orders-service/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/microsservico_orders', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected for Orders Service'))
-  .catch(err => console.log(err));
-
-module.exports = mongoose;
